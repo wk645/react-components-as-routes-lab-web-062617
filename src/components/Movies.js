6d205c65425877887dd7acd577f4ae9af049fe6f
wk645 <wk645@nyu.edu>
2017-09-01 00:22:26 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+        	return (
+        		<div>{movie.title} {movie.time}
+        			<ul>
+        				{movie.genres.map(genre => <li>{genre}</li>)}
+        			</ul>
+
+        		</div>
+    		)
+        })}
     </div>
   );
 };
